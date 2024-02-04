263f79748cd8597bd5ad7c26edfa16dd60046a8e
sitalbi <simon0111@live.fr>
2024-02-04 14:56:30 +0100

    leg movement working

--- Assets/LegMovement.cs
+++ Assets/LegMovement.cs
@@ -7,27 +7,41 @@ public class LegMovement : MonoBehaviour
 {
     [SerializeField] private Transform leg, target;
     [SerializeField] private float distanceDelta = 1f;
+    [SerializeField] private float stepHeight = 1f, speed = 10f;
 
-    private void Awake() {
-        transform.position = leg.position;
-    }
+    private float lerp;
+    private Vector3 newPos, currentPosition, oldPos;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        transform.position = leg.position;
+        newPos = target.position;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        CheckDistance();   
+        transform.position = currentPosition;
+        CheckDistance();
     }
-    
-    private void CheckDistance() {
-        float distance = Vector3.Distance(transform.position, target.position);
-        if(distance > distanceDelta) {
-            transform.position = target.position;
+
+    private void CheckDistance()
+    {
+        float distance = Vector3.Distance(newPos, target.position);
+        if (distance > distanceDelta) {
+            lerp = 0;
+            newPos = target.position;
+        }
+
+        if (lerp<1) {
+            Vector3 pos = Vector3.Lerp(oldPos, newPos, lerp);
+            pos.y += Mathf.Sin(lerp * Mathf.PI) * stepHeight;
+            
+            currentPosition = pos;
+            lerp += Time.deltaTime * speed;
+        }
+        else {
+            oldPos = newPos;
         }
     }
 }
