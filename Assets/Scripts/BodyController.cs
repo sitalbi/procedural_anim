48a23c9932ee06cda1d91a5f686bc3f1ac22e37b
sitalbi <simon0111@live.fr>
2024-02-08 15:48:43 +0100

    relief without rotation done

--- Assets/Scripts/BodyController.cs
+++ Assets/Scripts/BodyController.cs
@@ -1,13 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BodyController : MonoBehaviour
 {
+    [SerializeField] public float speed = 5f;
+    
     private List<Transform> targetTransforms;
     private float medianYPosition;
-    private Vector3 bodyPosition;
-    
+    private Vector3 bodyPosition, originalBodyPosition;
+
     void Awake()
     {
         targetTransforms = new List<Transform>();
@@ -16,22 +19,36 @@ public class BodyController : MonoBehaviour
                 targetTransforms.Add(t);
             }
         }
+        originalBodyPosition = transform.position;
     }
 
     
     void FixedUpdate() {
-        transform.position = MeanYPosition();
+        BodyPosition();
     }
     
-    private Vector3 MeanYPosition() {
-        // Get the mean Y position of the targets
-        float sum = 0;
+    /**
+     * Calculate the body position.
+     * X position updated with a constant speed
+     * Y position updated with the average y position of the targets
+     */
+    private void BodyPosition() {
+        // Calculate the mean y positions of the targets
+        medianYPosition = 0;
         foreach (Transform t in targetTransforms) {
-            sum += t.position.y;
+            medianYPosition += t.position.y;
         }
-        medianYPosition = sum / targetTransforms.Count;
-        return new Vector3(transform.position.x, transform.position.y + medianYPosition, transform.position.z);
+        medianYPosition /= targetTransforms.Count;
+        medianYPosition -= 0.5f; // Adjust the median pos with an arbitrary offset (might need to modify this)
+        
+        bodyPosition = new Vector3(transform.position.x + speed * Time.deltaTime, originalBodyPosition.y + medianYPosition, transform.position.z);
+        transform.position = bodyPosition;
     }
 
-
+    private void OnCollisionEnter(Collision collision) {
+        Debug.Log("Collision with " + collision.gameObject.name);
+        if(collision.gameObject.tag.Equals("Obstacle")) {
+            speed = -speed;
+        }
+    }
 }
--- Assets/Scripts/TargetPosManager.cs
+++ Assets/Scripts/TargetPosManager.cs
@@ -17,13 +17,13 @@ public class TargetPosManager : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        CalculateYPosition();
         CheckXDirection();
+        CalculateYPosition();
     }
 
     private void CheckXDirection() {
         // if the node is in front of this x, inverse the position x
-        if (node.position.x >= transform.position.x && !xDirection) {
+        if (node.position.x > transform.position.x && !xDirection) {
             transform.localPosition = new Vector3(-transform.localPosition.x, transform.localPosition.y, transform.localPosition.z);
             xDirection = true;
         } else if (node.position.x < transform.position.x && xDirection) {
@@ -34,11 +34,12 @@ public class TargetPosManager : MonoBehaviour
 
     private void CalculateYPosition() {
         // Use a downward raycast to find the ground (ground tag) and set the target position to the closest hit point (hits[hits.Length - 1])
-        RaycastHit[] hits;
-        hits = Physics.RaycastAll(transform.parent.position, Vector3.down, 10f, LayerMask.GetMask("Ground"));
-        transform.position = new Vector3(transform.position.x, hits[0].point.y, transform.position.z);
+        RaycastHit hit;
+        Vector3 rayOrigin = new Vector3(transform.position.x, transform.position.y + 2f, transform.position.z);
+        Physics.Raycast(rayOrigin, Vector3.down, out hit, 10f, LayerMask.GetMask("Ground"));
+        transform.position = new Vector3(transform.position.x, hit.point.y, transform.position.z);
         
         // Draw the raycast for debugging purposes
-        Debug.DrawRay(transform.parent.position, Vector3.down * 10f, Color.red);
+        Debug.DrawRay(rayOrigin, Vector3.down * 10f, Color.red);
     }
 }
